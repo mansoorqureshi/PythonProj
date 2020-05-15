7dd706162c5c9e5cab48c881d8ac40ec3404dd87
Mansoor Qureshi <mansoor_arshad@hotmail.com>
2020-05-15 21:33:20 +0500

    m

--- travello/views.py
+++ travello/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", {'price', 700})
+    return render(request, "index.html")
